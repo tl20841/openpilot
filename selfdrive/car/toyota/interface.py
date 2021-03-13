a12b1dda704c8a8eb6821b2d4667e593f8a19f1a
Shane Smiskol <shane@smiskol.com>
2021-03-12 19:48:24 -0600

    Use same tuning

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -111,11 +111,10 @@ class CarInterface(CarInterfaceBase):
       ret.minSpeedCan = 0.1 * CV.KPH_TO_MS
       tire_stiffness_factor = 0.444  # not optimized yet
       ret.mass = 2860. * CV.LB_TO_KG + STD_CARGO_KG  # mean between normal and hybrid
-      ret.lateralTuning.pid.kpBP, ret.lateralTuning.pid.kpV = [[20, 31], [0.04, 0.08]]  # 45 to 70 mph
-      ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kiV = [[20, 31], [0.001, 0.01]]
-      ret.lateralTuning.pid.kdBP, ret.lateralTuning.pid.kdV = [[20, 31], [0.1, 0.2]]
-      ret.lateralTuning.pid.kf = 0.00006908923778520113  # full torque for 20 deg at 80mph means 0.00007818594
-      ret.lateralTuning.pid.newKfTuned = True
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.08], [0.01]]
+      ret.lateralTuning.pid.kf = 0.00003  # full torque for 20 deg at 80mph means 0.00007818594
+      ret.lateralTuning.pid.kdBP, ret.lateralTuning.pid.kdV = [[0], [0]]
+      ret.lateralTuning.pid.newKfTuned = False
 
     elif candidate == CAR.LEXUS_RX:
       stop_and_go = True
